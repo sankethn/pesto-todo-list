865b72cdf8012431ea371d01302923d38e468cba
sankethn <hn.sanket2@gmail.com>
2024-07-07 21:30:21 +0530

    add env

--- pages/api/firebase.ts
+++ pages/api/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBP-J5jkrARmDoo4EKDB_S7un15QMM8x7A',
-  authDomain: 'pesto-project-b2d3a.firebaseapp.com',
-  databaseURL: 'https://pesto-project-b2d3a-default-rtdb.firebaseio.com',
-  projectId: 'pesto-project-b2d3a',
-  storageBucket: 'pesto-project-b2d3a.appspot.com',
-  messagingSenderId: '24188461454',
-  appId: '1:24188461454:web:06b7fc463f961e6faa04df',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
